a34526ebd1994e3ef2ebfea100c826457db6c648
Mike Robinson <mike.robinson@slicelife.com>
2022-02-09 11:09:59 +0000

    Adds url type apod:// to app Refacctors dependency injection container and model into app controller Adds onOpenUrl to SwiftUI App whic sets date on model triggering ui update

--- APOD/APOD/APODApp.swift
+++ APOD/APOD/APODApp.swift
@@ -4,12 +4,37 @@ import APODKit
 @main
 struct APODApp: App {
 
-    let dependencies = DependencyInjectionContainer()
+    let appController = AppController()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(APODModel(dependencies: dependencies))
+                .environmentObject(appController.model)
+                .onOpenURL { url in
+                    appController.openURL(url)
+                }
         }
+    }
+}
+
+class AppController {
+
+    let dependencies = DependencyInjectionContainer()
+
+    let model: APODModel
+
+    init() {
+        model = APODModel(dependencies: dependencies)
+    }
+
+    func openURL(_ url: URL) {
+        // parse url
+        guard let dateString = url.host,
+                let dateFromUrl = ISO8601DateFormatter.fullYearWithDashFormatter.date(from: dateString) else {
+            print("Error parsing date from url - \(url)")
+            return
+        }
+
+        model.date = dateFromUrl
     }
 }
